80cdccb55c548692abd8612cc7ab3d8d17b88830
Ross Jameson <Rossjameson1623@gmail.com>
2019-10-20 12:05:37 -0500

    adding delete functionality and adding photos

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,57 @@
 import React, { Component } from 'react';
 
+import bran from '../images/bran.jpg';
+
+
          
 
 class Counter extends Component {
     state = {
-            count: 0,
-            tags: ['tag1', 'tag2', 'tag3']
-             // imageUrl: 'https://picsum.photos/200' 
+            value: this.props.counter.value,
+            tags: ['tag1', 'tag2', 'tag3'],
+             imageUrl: '../images/bran.jpg' 
 };
 
 
 renderTags(){
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    if (this.state.tags.length === 0) return <p>There are no tags
+
+    </p>;
 
     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
 }
 
-constructor(){
-    super();
-    console.log('Constructor', this);
-}
-    handleIncrement(){
-        console.log('Increment Clicked');
-    }
+// constructor(){
+//     super();
+//   this.handleIncrement = this.handleIncrement.bind(this);
+// }
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({ value: this.state.value + 1});
+    };
 
     render() {
-       
+     
+        
         return (
         <div>
-        {/* <img src={this.state.imageUrl} alt=""/> */}
-        <span style = {{fontSize: 30}} className={this.getBadgeClasses()}>{this.formatCount()}
+            <h4>{this.props.id}</h4>
+        <img src={bran} alt=""/>
+        <span style = {{fontSize: 30}}
+         className={this.getBadgeClasses()}>{this.formatCount()}
         </span>
 
-        <button onClick = {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button 
+        onClick ={() => this.handleIncrement({id:1})} 
+        className="btn btn-secondary btn-sm">
+            Increment
+            </button>
     
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        {this.renderTags()}
+    <button onClick={() => this.props.onDelete(this.props.counter.id)}className="btn btn-danger btn-sm m-2">Delete</button>
+
+        {/* {this.state.tags.length === 0 && "Please create a new tag"} */}
+        {/* below creates a list below each button */}
+        {/* {this.renderTags()} */}
 
     </div>
         );
@@ -44,12 +60,12 @@ constructor(){
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
 }
